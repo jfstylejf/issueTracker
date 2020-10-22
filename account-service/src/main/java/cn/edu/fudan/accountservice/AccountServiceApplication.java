0e083b7eedf88c6c1d3cd93cbb92544fe9f75743
Zrq-Q <yian02250316@163.com>
2020-10-22 21:52:06 +0800

    add swagger

--- account-service/src/main/java/cn/edu/fudan/accountservice/AccountServiceApplication.java
+++ account-service/src/main/java/cn/edu/fudan/accountservice/AccountServiceApplication.java
@@ -3,7 +3,9 @@ package cn.edu.fudan.accountservice;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 @MapperScan("cn.edu.fudan.accountservice.mapper")
 public class AccountServiceApplication {
--- account-service/src/main/java/cn/edu/fudan/accountservice/controller/AccountController.java
+++ account-service/src/main/java/cn/edu/fudan/accountservice/controller/AccountController.java
@@ -5,11 +5,20 @@ import cn.edu.fudan.accountservice.domain.ResponseBean;
 import cn.edu.fudan.accountservice.domain.Tool;
 import cn.edu.fudan.accountservice.service.AccountService;
 import cn.edu.fudan.accountservice.util.CookieUtil;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
+
+//@ApiOperation注解 value添加该api的说明，用note表示该api的data返回类型，httpMethod表示请求方式
+//@ApiImplicitParams 参数列表集合注解
+//@ApiImplicitParam 参数说明接口 对应api中@RequestParam作解释说明 可显示在swagger2-ui页面上
+//  name表示参数名 value表示对参数的中文解释 dataType表示该参数类型 required表示该参数是否必须 defaultValue提供测试样例
+//  具体@ApiImplicitParam其他属性ctrl+click 点进源码
 
 
 @RestController
@@ -24,31 +33,52 @@ public class AccountController {
         this.accountService = accountService;
     }
 
+    @ApiOperation(value="获取用户名并检查",notes="@return boolean",httpMethod = "GET")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "accountName", value = "开发人员姓名", dataType = "String", required = true,defaultValue = "lluo"),
+    })
     @GetMapping("/account-name/check")
     public Object checkUserName(@RequestParam("accountName") String accountName) {
         return new ResponseBean(200, "success", accountService.isAccountNameExist(accountName));
     }
 
+    @ApiOperation(value="获取用户邮箱并检查",notes="@return boolean",httpMethod = "GET")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "email", value = "开发人员邮箱", dataType = "String", required = true,defaultValue = "chjunlm@126.com"),
+    })
     @GetMapping("/email/check")
     public Object checkEmail(@RequestParam("email") String email) {
         return new ResponseBean(200, "success", accountService.isEmailExist(email));
     }
 
+    @ApiOperation(value="获取用户昵称并检查",notes="@return boolean",httpMethod = "GET")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "nickName", value = "开发人员昵称", dataType = "String", required = true,defaultValue = "王露"),
+    })
     @GetMapping("/nick-name/check")
     public Object checkNickName(@RequestParam("nickName") String nickName) {
         return new ResponseBean(200, "success", accountService.isNameExist(nickName));
     }
 
+    @ApiOperation(value="获取用户在职状态",notes="@return Object",httpMethod = "GET")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "name", value = "开发人员姓名列表", dataType = "List<String>", required = true,defaultValue = "[\"admin1\",\"罗理\",\"程君\"]"),
+    })
     @GetMapping("/status")
     public Object getStatusByName(@RequestBody List<String> name) {
         return new ResponseBean(200, " ",accountService.getStatusByName(name));
     }
 
+    @ApiOperation(value="获取用户信息",notes="@return List<Account>",httpMethod = "GET")
     @GetMapping("/status/getData")
     public Object getAccountStatus(){
         return new ResponseBean(200, " ",accountService.getAccountStatus());
     }
 
+    @ApiOperation(value="更改用户角色、部门、在职状态",httpMethod = "PUT")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "statusInfo", value = "用户角色、部门、在职状态", dataType = "List<Account>", required = true,defaultValue = "[\"admin1\",\"罗理\",\"程君\"]"),
+    })
     @PutMapping(value = "/status/dep/role")
     public Object updateAccountStatus(@RequestBody List<Account> statusInfo){
         try{
@@ -60,6 +90,19 @@ public class AccountController {
 
     }
 
+    @ApiOperation(value="用户注册",httpMethod = "POST")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "account", value = "开发人员信息列表", dataType = "Account", required = true,defaultValue = "{\n" +
+                    "    \"accountName\":\"zwj\",\n" +
+                    "    \"password\":\"123456\",\n" +
+                    "    \"name\":\"周玮杰\",\n" +
+                    "    \"email\":\"zwj@163.com\",\n" +
+                    "    \"dep\":\"工程开发部\",\n" +
+                    "    \"gitname\":\"zwj\",\n" +
+                    "    \"status\":1,\n" +
+                    "    \"role\":\"P\"\n" +
+                    "}"),
+    })
     @PostMapping("/register")
     public Object createUser(@RequestBody Account account) {
         try {
@@ -70,6 +113,11 @@ public class AccountController {
         }
     }
 
+    @ApiOperation(value="注册",notes="@return AccountInfo",httpMethod = "GET")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "username", value = "用户姓名", dataType = "String", required = true,defaultValue = "admin1"),
+            @ApiImplicitParam(name = "password", value = "密码", dataType = "String", required = true,defaultValue = "YWRtaW4x"),
+    })
     @GetMapping(value = {"/login"})
     public Object login(@RequestParam("username") String username, @RequestParam("password") String password, HttpServletResponse response) {
         ResponseBean responseBean = accountService.login(username, password);
@@ -79,11 +127,19 @@ public class AccountController {
         return responseBean;
     }
 
+    @ApiOperation(value="获取当前登录用户的uuid",notes="@return Account Uuid",httpMethod = "GET")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "userToken", value = "用户token", dataType = "String", required = true,defaultValue = "378f71b14cc4a0f1e6ce18bfe3a4028e"),
+    })
     @GetMapping(value = "/accountId")
     public Object getAccountID(@RequestParam("userToken") String userToken) {
         return new ResponseBean(200, "success", accountService.getAccountByToken(userToken).getUuid());
     }
 
+    @ApiOperation(value="获取当前登录用户登录状态",httpMethod = "GET")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "userToken", value = "用户token", dataType = "String", required = true,defaultValue = "378f71b14cc4a0f1e6ce18bfe3a4028e"),
+    })
     @GetMapping(value = "/auth/{userToken}")
     public Object auth(@PathVariable("userToken") String userToken) {
         if (accountService.authByToken(userToken)) {
@@ -93,6 +149,10 @@ public class AccountController {
         }
     }
 
+    @ApiOperation(value="获取当前登录用户权限",notes="@return Map<String,Object>",httpMethod = "GET")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "userToken", value = "用户token", dataType = "String", required = true,defaultValue = "378f71b14cc4a0f1e6ce18bfe3a4028e"),
+    })
     @GetMapping(value = "/right/{userToken}")
     @CrossOrigin
     public Object right(@PathVariable("userToken") String userToken) {
@@ -103,16 +163,25 @@ public class AccountController {
         }
     }
 
+    @ApiOperation(value="获取用户ID",notes="@return List<String>",httpMethod = "GET")
     @GetMapping(value = "/accountIds")
     public Object getAccountIds() {
         return new ResponseBean(200, "success", accountService.getAllAccountId());
     }
 
+    @ApiOperation(value="通过姓名获取用户组别",notes="@return List<String>",httpMethod = "GET")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "accountName", value = "开发人员姓名", dataType = "String", required = true,defaultValue = "lluo"),
+    })
     @GetMapping(value = "/accountGroups")
     public Object getGroupsByAccountName(@RequestParam("accountName") String accountName){
         return new ResponseBean(200, "success",accountService.getGroupsByAccountName(accountName));
     }
 
+    @ApiOperation(value="更新工具",httpMethod = "POST")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "tools", value = "工具", dataType = "List<Tool>", required = true,defaultValue = "[\"sonarqube\",\"findbugs\"]"),
+    })
     @PostMapping(value = "/updateTools")
     public ResponseBean updateToolsEnable(@RequestBody List<Tool> tools){
         try{
@@ -123,11 +192,16 @@ public class AccountController {
         }
     }
 
+    @ApiOperation(value="获取工具列表",notes="@return List<Tool>",httpMethod = "GET")
     @GetMapping(value = "/tools")
     public Object getTools(){
         return new ResponseBean(200, "success",accountService.getTools());
     }
 
+    @ApiOperation(value="获取用户姓名",httpMethod = "GET")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "accountId", value = "用户ID", dataType = "String", required = true,defaultValue = "1"),
+    })
     @GetMapping(value = "/accountName")
     public Object getAccountNameById(@RequestParam("accountId") String accountId){
         return new ResponseBean(200, "success",accountService.getAccountNameById(accountId));
