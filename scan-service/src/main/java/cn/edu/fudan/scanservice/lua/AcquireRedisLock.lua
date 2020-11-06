9ea19c788ec52283582de90bc8ad8ba8a4fab726
jfstylejf <15801850335@163.com>
2020-11-06 15:36:08 +0800

    merge scan-service

--- scan-service/src/main/java/cn/edu/fudan/scanservice/lua/AcquireRedisLock.lua
+++
@@ -1,6 +0,0 @@
-if redis.call('exists',KEYS[1]) == 0
-    then redis.call('setex',KEYS[1],ARGV[1],ARGV[2])
-return true
-end
-return false
-
--- scan-service/src/main/java/cn/edu/fudan/scanservice/lua/ReleaseRedisLock.lua
+++
@@ -1,3 +0,0 @@
-if redis.call('get',KEYS[1]) == ARGV[1]
-then return redis.call('del',KEYS[1]) or true
-end
