35a191b1e0ada336874cf92a8cd0c6a511124235
shaoxi <shaoxi277214@foxmail.com>
2021-03-03 09:53:27 +0800

    write sh

--- depend-service/src/main/java/cn/edu/fudan/dependservice/controller/PutDependController.java
+++ depend-service/src/main/java/cn/edu/fudan/dependservice/controller/PutDependController.java
@@ -1,20 +1,41 @@
 package cn.edu.fudan.dependservice.controller;
 
 import cn.edu.fudan.dependservice.impl.RunnerImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PutDependController {
 
+    private ApplicationContext applicationContext;
+
     @RequestMapping(value = {"/putDepend"}, method = RequestMethod.GET)
-    public String put2Database() {
+    //String repoPath
+    public String put2Database(@RequestParam(name = "repoPath") String repoPath) {
+        System.out.println();
         // if ok return true else return false;
-        RunnerImpl runner=new RunnerImpl();
-        runner.runTool("tempRepoPath","tempCommitID");
-        return "true";
+        StringBuilder res=new StringBuilder();
+        res.append("now dir:"+System.getProperty("user.dir"));
+        res.append("\n");
+
+        res.append("repoPath:"+repoPath);
+        res.append("\n");
+        repoPath="/home/fdse/codeWisdom/service/dependence-analysis/scenario-engine";
+        RunnerImpl runner=new RunnerImpl(applicationContext);
+
+//        runner.setShHome("D:\\allIdea\\IssueTracker-test\\depend-service\\src\\main\\java\\cn\\edu\\fudan\\dependservice\\");
+        //D:\allIdea\IssueTracker-test\depend-service\src\main\java\cn\edu\fudan\dependservice
+        runner.runTool(repoPath,"tempCommitID");
+        return res.toString();
         //
     }
 
+    @Autowired
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
 }
--- depend-service/src/main/java/cn/edu/fudan/dependservice/impl/RunnerImpl.java
+++ depend-service/src/main/java/cn/edu/fudan/dependservice/impl/RunnerImpl.java
@@ -1,30 +1,49 @@
 package cn.edu.fudan.dependservice.impl;
 
 import cn.edu.fudan.dependservice.Runner;
+import cn.edu.fudan.dependservice.config.ShHomeConfig;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationContext;
 
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
+@Data
 public class RunnerImpl implements Runner {
     private String shHome;
     private String shName;
-
-    public String getShHome() {
-        return shHome;
+    private String repoPath="/home/fdse/codeWisdom/service/dependence-analysis/scenario-engine";
+    private String dependenceHome;
+    public RunnerImpl(ApplicationContext applicationContext){
+        this.dependenceHome = applicationContext.getBean(ShHomeConfig.class).getDependenceHome();
+        this.shHome=dependenceHome;
+        this.setShName("tdepend.sh");
     }
 
-    public void setShHome(String shHome) {
-        this.shHome = shHome;
-    }
-
-    public String getShName() {
-        return shName;
-    }
-
-    public void setShName(String shName) {
-        this.shName = shName;
-    }
+//    public String getRepoPath() {
+//        return repoPath;
+//    }
+//
+//    public void setRepoPath(String repoPath) {
+//        this.repoPath = repoPath;
+//    }
+//
+//    public String getShHome() {
+//        return shHome;
+//    }
+//
+//    public void setShHome(String shHome) {
+//        this.shHome = shHome;
+//    }
+//
+//    public String getShName() {
+//        return shName;
+//    }
+//
+//    public void setShName(String shName) {
+//        this.shName = shName;
+//    }
 
     @Override
     public void runTool(String repoPath, String commitID) {
@@ -35,8 +54,10 @@ public class RunnerImpl implements Runner {
     public boolean invoke(String repoPath, String commitID) {
         try {
             Runtime rt = Runtime.getRuntime();
-            String command =shHome+shName+" "+repoPath+" "+commitID;
+//            String command ="sh "+shHome+shName+" "+repoPath+" "+commitID;
+            String command ="sh "+shHome+shName+" "+repoPath;
             log.info("command -> {}",command);
+
             Process process = rt.exec(command);
             //最多等待sonar脚本执行200秒,超时则认为该commit解析失败
             boolean timeout = process.waitFor(300L, TimeUnit.SECONDS);
@@ -50,17 +71,5 @@ public class RunnerImpl implements Runner {
             e.printStackTrace();
         }
         return false;
-    }
-
-
-    public static void main(String[] args) {
-        RunnerImpl runner =new RunnerImpl();
-        // can not test in win
-        runner.setShHome("D:\\allIdea\\IssueTracker-test\\depend-service\\src\\main\\java\\cn\\edu\\fudan\\dependservice\\");
-        //D:\allIdea\IssueTracker-test\depend-service\src\main\java\cn\edu\fudan\dependservice
-        runner.setShName("depend.sh");
-        runner.runTool("tempRepo","tempCommitId");
-
-
     }
 }
