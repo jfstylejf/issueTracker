4a641fe91bf772e4e9ae103fa3c8f577234733ff
jfstylejf <15801850335@163.com>
2021-03-29 14:15:34 +0800

    增加扫描前睡眠时间，2分钟内手动修改gitlab的repo的language为js

--- scan-service/src/main/java/cn/edu/fudan/scanservice/service/InvokeToolService.java
+++ scan-service/src/main/java/cn/edu/fudan/scanservice/service/InvokeToolService.java
@@ -37,7 +37,7 @@ public class InvokeToolService {
         final int enabled = 1;
         Map<Integer,String> preToolInvokeMap = new HashMap<> ();
 
-        //第一步 先获取当前repo 之前的调用情况
+        // 第一步 先获取当前repo 之前的调用情况
         Scan preScan = scanDao.getScanByRepoId (repoId);
         if(startCommit == null && preScan == null){
             log.error (" repo id --> {} ,invoke tools error, cause startCommit is null and has not been scanned!", repoId);
@@ -57,10 +57,10 @@ public class InvokeToolService {
 
         // todo 后续基础服务能够正常判断language时，要把这个sleep去掉
         try {
-            //        log.info("开始睡眠1分钟，请至数据库中手动修改刚刚添加repo的language。。。");
-//        Thread.currentThread().sleep(180000);
-//        log.info("下面开始进行各个工具的扫描请求!");
-            //第二步根据之前的调用结果，采取相应的调用方式
+            log.info("开始睡眠2分钟，请至数据库中手动修改刚刚添加repo的language...");
+            Thread.sleep(120000);
+            log.info("下面开始进行各个工具的扫描请求!");
+        // 第二步根据之前的调用结果，采取相应的调用方式
         }catch (Exception e) {
             log.error("time sleep error");
             log.error(e.getMessage());
