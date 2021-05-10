3850ffa4d538c8e01c54432622aec7cc568a9e44
支乌 <beethoven@zhiwudeMacBook-Pro.local>
2021-05-10 10:21:02 +0800

    update issue  delete

--- issue-service/src/main/java/cn/edu/fudan/issueservice/service/impl/IssueServiceImpl.java
+++ issue-service/src/main/java/cn/edu/fudan/issueservice/service/impl/IssueServiceImpl.java
@@ -81,6 +81,13 @@ public class IssueServiceImpl implements IssueService {
         issueRepoDao.delIssueRepo(repoUuid, null, tool);
         issueScanDao.deleteIssueScanByRepoIdAndTool(repoUuid, tool);
         scanResultDao.deleteScanResultsByRepoIdAndCategory(repoUuid, tool);
+
+        //check delete success
+        boolean success = issueDao.checkDeleteSuccess(repoUuid, tool);
+        if (success) {
+            restInterfaceManager.sendDeleteSuccessMessage(repoUuid);
+        }
+
         //完成删库
         logger.info("finish deleting issues -> repoUuid={} , tool={}", repoUuid, tool);
     }
