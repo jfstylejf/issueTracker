ad067afdb612e3382df33b2ec24a0f2df907b39a
shaoxi <shaoxi277214@foxmail.com>
2021-05-11 09:58:05 +0800

    start DeleteController

---
+++ depend-service/src/main/java/cn/edu/fudan/dependservice/controller/DeleteController.java
@@ -0,0 +1,13 @@
+package cn.edu.fudan.dependservice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author shaoxi
+ * @version 0.1.0
+ * @create 2021-05-10 16:30
+ **/
+@RestController
+
+public class DeleteController {
+}
